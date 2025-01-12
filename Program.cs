bee2cecda9792d3910c252aeb26d1220a344c5c0
carlos50311 <geraldocarlos503@gmail.com>
2025-01-11 23:48:28 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo quer contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplicador = 1;
+
+            if (type == 'm')
+                multiplicador = 60;
+
+            if (type == '0')
+                System.Environment.Exit(0);
+
+            Start(time * multiplicador);
+        }
+
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch finaizado ");
+            Thread.Sleep(2500);
+            Menu();
+        }
+
+    }
+}
